bd66257832d6d00562405b86c3575cbd6fc927b2
ghanbarim <ghanbari@stanford.edu>
2015-04-26 18:46:52 -0700

    Basic newsfeed layout

--- SocialMe/SocialMe/FeedVC.swift
+++ SocialMe/SocialMe/FeedVC.swift
@@ -9,13 +9,20 @@
 import UIKit
 import Parse
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    let tableView = UITableView()
+    var data = NSArray()
+    
     @IBAction func ShowProfile(sender: UIBarButtonItem) {
         println("Profile Tab")
         navigationController?.setNavigationBarHidden(true, animated: true)
         let tab = tabBarController!.tabBar
         UIView.animateWithDuration(0.2, animations: {
             tab.frame.origin = CGPointMake(0, self.view.frame.height)
+            self.tableView.frame.origin = CGPointMake(0, self.view.frame.height - 50)
+            
             }, completion: {
                 (value: Bool) in
                 self.addSwipeUp()
@@ -27,7 +34,7 @@ class FeedVC: UIViewController {
         let swipeView = UIView(frame: CGRectMake(0, view.frame.height - 50, view.frame.width, 50))
         let swipe = UISwipeGestureRecognizer(target: self, action: "swipeUp:")
         swipe.direction = .Up
-        
+        view.backgroundColor = UIColor.orangeColor()
         let tap = UITapGestureRecognizer(target: self, action: "swipeUp:")
         swipeView.userInteractionEnabled = true
         swipeView.addGestureRecognizer(swipe)
@@ -41,6 +48,7 @@ class FeedVC: UIViewController {
         let tab = tabBarController!.tabBar
         UIView.animateWithDuration(0.2, animations: {
             tab.frame.origin = CGPointMake(0, self.view.frame.height - 50)
+            self.tableView.frame.origin = CGPointMake(0, 0)
             }, completion: {
                 (value: Bool) in
                 sender.view?.removeFromSuperview()
@@ -48,9 +56,64 @@ class FeedVC: UIViewController {
         
     }
     
+    func initTableView() {
+        tableView.frame = view.frame
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "cell") //tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let user = data[indexPath.row] as! PFUser
+        println(user)
+        cell.textLabel!.text = user.username
+        cell.detailTextLabel!.text = user.email
+        cell.imageView?.image = UIImage(named: "podcasts")
+        cell.separatorInset = UIEdgeInsetsZero
+        // cell.contentView.backgroundColor = UIColor.grayColor()
+        return cell
+    }
+    
+    func getAllUsers() {
+        let value = NSArray()
+        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            
+            var query = PFUser.query()
+            query!.whereKey("username", notEqualTo:"blah")
+            var res : NSArray = query!.findObjects()!
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                self.data = res
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println(indexPath.row)
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
     @IBAction func logout(sender: UIButton) {
         PFUser.logOut()
         presentMainVC()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initTableView()
+        getAllUsers()
     }
     
     override func viewDidAppear(animated: Bool) {
