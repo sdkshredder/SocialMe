d659a792d9a3a94345d55289c9ec1d3c5b371455
ghanbarim <ghanbari@stanford.edu>
2015-04-26 17:29:50 -0700

    Swipe Up

--- SocialMe/SocialMe/FeedVC.swift
+++ SocialMe/SocialMe/FeedVC.swift
@@ -10,6 +10,43 @@ import UIKit
 import Parse
 
 class FeedVC: UIViewController {
+    @IBAction func ShowProfile(sender: UIBarButtonItem) {
+        println("Profile Tab")
+        navigationController?.setNavigationBarHidden(true, animated: true)
+        let tab = tabBarController!.tabBar
+        UIView.animateWithDuration(0.2, animations: {
+            tab.frame.origin = CGPointMake(0, self.view.frame.height)
+            }, completion: {
+                (value: Bool) in
+                self.addSwipeUp()
+        })
+        
+    }
+    
+    func addSwipeUp() {
+        let swipeView = UIView(frame: CGRectMake(0, view.frame.height - 50, view.frame.width, 50))
+        let swipe = UISwipeGestureRecognizer(target: self, action: "swipeUp:")
+        swipe.direction = .Up
+        
+        let tap = UITapGestureRecognizer(target: self, action: "swipeUp:")
+        swipeView.userInteractionEnabled = true
+        swipeView.addGestureRecognizer(swipe)
+        swipeView.addGestureRecognizer(tap)
+        view.addSubview(swipeView)
+        
+    }
+    
+    func swipeUp(sender: UISwipeGestureRecognizer) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        let tab = tabBarController!.tabBar
+        UIView.animateWithDuration(0.2, animations: {
+            tab.frame.origin = CGPointMake(0, self.view.frame.height - 50)
+            }, completion: {
+                (value: Bool) in
+                sender.view?.removeFromSuperview()
+        })
+        
+    }
     
     @IBAction func logout(sender: UIButton) {
         PFUser.logOut()
