6585095f3b496779adbfb498298df5b3fb9dd81b
duhamel <mduhamel@stanford.edu>
2015-05-17 14:51:35 -0700

    hello

---
+++ SocialMe/SocialMe/ProfilePictureTVCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfilePictureTVCell.swift
+//  SocialMe
+//
+//  Created by Matt Duhamel on 5/17/15.
+//  Copyright (c) 2015 new. All rights reserved.
+//
+
+import UIKit
+
+class ProfilePictureTVCell: UITableViewCell {
+
+}
