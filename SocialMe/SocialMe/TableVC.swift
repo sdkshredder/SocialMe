e1c075cd737302602546fe05d098065e69cabf3a
duhamel <mduhamel@stanford.edu>
2015-05-23 16:49:53 -0700

    updates and improvements

--- SocialMe/SocialMe/TableVC.swift
+++ SocialMe/SocialMe/TableVC.swift
@@ -17,27 +17,74 @@ class TableVC: UIViewController, UITableViewDelegate, UITableViewDataSource, CLL
     let locationManager = CLLocationManager()
     let picker = UIImagePickerController()
     
+    @IBOutlet weak var constraint: NSLayoutConstraint!
     @IBOutlet weak var table: UITableView!
     var editingAge = false
     
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        let a = getTitleForPath(indexPath.section)
-        if a == "Age" && editingAge == true {
-            return 220
-        } else if a == "Location" {
-            return 90
-        } else if a == "Profile Picture" {
-            return 150
-        } else if a == "Log Out" {
-            return 50
-        }
-        return 60
-    }
-    
     func colorTable() {
         for subview in table.subviews {
-            subview.contentView.backgroundColor = UIColor.blueColor() // = UIColor.blueColor()
+            subview.contentView.backgroundColor = UIColor.blueColor()
         }
+    }
+    
+    func registerNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"handleNotification:", name: "ageNotification", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"handleImageNotification:", name: "imageNotification", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleKeyboardWillShowNotification:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleKeyboardWillHideNotification:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func handleKeyboardWillShowNotification(notification: NSNotification) {
+        keyboardWillChangeFrameWithNotification(notification, showsKeyboard: true)
+    }
+    
+    func handleKeyboardWillHideNotification(notification: NSNotification) {
+        keyboardWillChangeFrameWithNotification(notification, showsKeyboard: false)
+    }
+    
+    func keyboardWillChangeFrameWithNotification(notification: NSNotification, showsKeyboard: Bool) {
+        let userInfo = notification.userInfo!
+        
+        let animationDuration: NSTimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+        
+        let keyboardScreenBeginFrame = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        
+        let keyboardViewBeginFrame = view.convertRect(keyboardScreenBeginFrame, fromView: view.window)
+        let keyboardViewEndFrame = view.convertRect(keyboardScreenEndFrame, fromView: view.window)
+        var originDelta = keyboardViewEndFrame.origin.y - keyboardViewBeginFrame.origin.y
+        
+        // constraint.constant -= originDelta
+        
+        // view.setNeedsUpdateConstraints()
+        
+        if showsKeyboard == false {
+            self.table.frame.size.height = 470
+            //self.table.frame.height // = originDelta
+        } else {
+                            var height = self.table.frame.size.height
+                            println(height)
+                            let a = height + originDelta + 50
+                            self.table.frame.size.height = a
+        }
+        
+        UIView.animateWithDuration(animationDuration, delay: 0, options: .BeginFromCurrentState, animations: {
+            //self.view.layoutIfNeeded()
+            
+            if showsKeyboard == false {
+                
+                //self.table.frame.height // = originDelta
+            } else {
+//                var height = self.table.frame.size.height
+//                println(height)
+//                let a = height + originDelta + 50
+//                self.table.frame.size.height = a
+            }
+            }, completion: nil)
+        
+        // Scroll to the selected text once the keyboard frame changes.
+        //let selectedRange = textView.selectedRange
+        //textView.scrollRangeToVisible(selectedRange)
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -48,26 +95,6 @@ class TableVC: UIViewController, UITableViewDelegate, UITableViewDataSource, CLL
         return 7
     }
     
-    func getTitleForPath(path : Int) -> String {
-        switch path {
-            case 0:
-                return "Profile Picture"
-            case 1:
-                return "Name"
-            case 2:
-                return "Age"
-            case 3:
-                return "Hometown"
-            case 4:
-                return "School"
-            case 5:
-                return "Occupation"
-            case 6:
-                return "Location"
-        default:
-                return "Log Out"
-        }
-    }
     
     @IBAction func cancel(sender: UIBarButtonItem) {
         NSNotificationCenter.defaultCenter().postNotificationName("hideSettings", object: nil)
@@ -77,7 +104,11 @@ class TableVC: UIViewController, UITableViewDelegate, UITableViewDataSource, CLL
         let a = getTitleForPath(path)
         if a == "Age" {
             return "ageCell"
-        } else if a == "Profile Picture"{
+        } else if a == "" {
+            return "logoutCell"
+        }
+        
+        else if a == "Profile Picture"{
             return "profilePictureCell"
         }
         else if a == "Location" {
@@ -128,9 +159,7 @@ class TableVC: UIViewController, UITableViewDelegate, UITableViewDataSource, CLL
             
             return cell
         } else if ID == "logoutCell" {
-            var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "logout")//tableView.dequeueReusableCellWithIdentifier(ID) as! UITableViewCell //dequeueReusableCellWithIdentifier(ID) as! ProfilePictureTVCell
-            cell.textLabel!.text = "Log Out"
-
+            var cell = tableView.dequeueReusableCellWithIdentifier(ID) as! LogOutTVCell
             return cell
         }
         
@@ -147,14 +176,6 @@ class TableVC: UIViewController, UITableViewDelegate, UITableViewDataSource, CLL
             if let val = user?.objectForKey(attr) as? String {
                 cell.textField.attributedPlaceholder = NSAttributedString(string: val, attributes: [NSForegroundColorAttributeName : UIColor.darkGrayColor()])
             }
-
-            /*
-            let val = user?.objectForKey(attr) as! String
-   
-            if !val.isEmpty {
-                cell.textField.attributedPlaceholder = NSAttributedString(string: val, attributes: [NSForegroundColorAttributeName : UIColor.darkGrayColor()])
-            }
-            */
             return cell
         } else if ID == "ageCell" {
             var b = tableView.dequeueReusableCellWithIdentifier(ID) as! AgeTVCell
@@ -176,31 +197,36 @@ class TableVC: UIViewController, UITableViewDelegate, UITableViewDataSource, CLL
         super.viewDidLoad()
         table.delegate = self
         table.dataSource = self
-    
-        NSNotificationCenter.defaultCenter().addObserver(self, selector:"handleNotification:", name: "ageNotification", object: nil)
-        
-        NSNotificationCenter.defaultCenter().addObserver(self, selector:"handleImageNotification:", name: "imageNotification", object: nil)
+        registerNotifications()
     }
     
-    func handleImageNotification(notification: NSNotification) {
-        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+    func presentPicker(type : UIImagePickerControllerSourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(type) {
             let picker = UIImagePickerController()
-            picker.sourceType = .Camera
-            picker.showsCameraControls = true
-            picker.mediaTypes = [kUTTypeImage]
+            picker.sourceType = type
+            if type == .Camera {
+                picker.showsCameraControls = true
+                picker.mediaTypes = [kUTTypeImage]
+            }
             picker.delegate = self
             picker.allowsEditing = true
-            let button = UIButton(frame: CGRectMake(100, 100, 100, 100))
-            button.backgroundColor = UIColor.greenColor()
-            button.addTarget(self, action: "pickPic:", forControlEvents: UIControlEvents.TouchUpInside)
-            //picker.insertSubview(button, aboveSubview: picker.view)
-            
             presentViewController(picker, animated: true, completion: nil)
         }
     }
     
-    func pickPic(sender: UITapGestureRecognizer) {
-        println("piiiimp")
+    func handleImageNotification(notification: NSNotification) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+        actionSheet.addAction(UIAlertAction(title:"Upload Photo", style:UIAlertActionStyle.Default, handler:{ action in
+            let type = UIImagePickerControllerSourceType.PhotoLibrary
+            self.presentPicker(type)
+        }))
+        actionSheet.addAction(UIAlertAction(title:"Take a Picture", style:UIAlertActionStyle.Default, handler:{ action in
+            let type = UIImagePickerControllerSourceType.Camera
+            self.presentPicker(type)
+        }))
+        actionSheet.addAction(UIAlertAction(title:"Cancel", style:UIAlertActionStyle.Cancel, handler:nil))
+        
+        presentViewController(actionSheet, animated:true, completion:nil)
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
@@ -216,9 +242,9 @@ class TableVC: UIViewController, UITableViewDelegate, UITableViewDataSource, CLL
         currentUser!.saveInBackgroundWithBlock{
             (succeeded, error) -> Void in
             if error == nil {
-                println("successsssss for user \(currentUser!.username)")
+                println("image saved")
             } else {
-                println(" error")
+                println("error saving image")
             }
     
         }
@@ -231,17 +257,44 @@ class TableVC: UIViewController, UITableViewDelegate, UITableViewDataSource, CLL
         dismissViewControllerAnimated(true, completion: nil)
         NSNotificationCenter.defaultCenter().postNotificationName("hideSettings", object: nil)
     }
-    
-    /*
-    func imagePickerControllerDidCancel(UIIPC) {
-        presentingViewController.dismissViewControllerAnimated(true, completion: nil) }
-    }
-    */
 
     func handleNotification(notification: NSNotification) {
         editingAge = !editingAge
         table.beginUpdates()
         table.endUpdates()
         NSNotificationCenter.defaultCenter().postNotificationName("pickerNotification", object: nil)
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let a = getTitleForPath(indexPath.section)
+        if a == "Age" && editingAge == true {
+            return 340
+        } else if a == "Location" {
+            return 90
+        } else if a == "Profile Picture" {
+            return 240
+        } else if a == "Log Out" {
+            return 50
+        }
+        return 60
+    }
+    
+    func getTitleForPath(path : Int) -> String {
+        switch path {
+        case 0:
+            return "Profile Picture"
+        case 1:
+            return "Name"
+        case 2:
+            return "Hometown"
+        case 3:
+            return "School"
+        case 4:
+            return "Occupation"
+        case 5:
+            return "Location"
+        default:
+            return ""
+        }
     }
 }
