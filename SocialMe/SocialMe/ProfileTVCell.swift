e26e2f643ef902d8a6d286883801d958e05ca5d6
duhamel <mduhamel@stanford.edu>
2015-05-23 20:55:18 -0700

    on the same page :P

---
+++ SocialMe/SocialMe/ProfileTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileTVC.swift
+//  SocialMe
+//
+//  Created by Kelsey Young on 5/1/15.
+//  Copyright (c) 2015 new. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTVCell: UITableViewCell, UITextFieldDelegate {
+
+    @IBOutlet weak var attrLabel: UILabel!
+    @IBOutlet weak var inputLabel: UILabel!
+    
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        println("Hey")
+        // Configure the view for the selected state
+    }
+    
+}
