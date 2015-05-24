e1c7d63ed216bb7e45d3e614a8c07039e9f9717b
duhamel <mduhamel@stanford.edu>
2015-05-23 19:39:13 -0700

    fixed feed logic

--- SocialMe/SocialMe/FeedVC.swift
+++ SocialMe/SocialMe/FeedVC.swift
@@ -16,7 +16,6 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var data = NSArray()
     
     @IBAction func ShowProfile(sender: UIBarButtonItem) {
-        //println("Profile Tab")
         navigationController?.setNavigationBarHidden(true, animated: true)
         let tab = tabBarController!.tabBar
         UIView.animateWithDuration(0.2, animations: {
@@ -84,119 +83,56 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "cell") //tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
-        
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "cell")
         let user = data[indexPath.row] as! PFUser
-        //println(user)
-        /*
-        cell.textLabel!.text = user.username
-        cell.detailTextLabel!.text = user.email
-        cell.imageView?.image = UIImage(named: "podcasts")
+        styleCell(cell.contentView, user: user)
         cell.separatorInset = UIEdgeInsetsZero
-        
         cell.layoutMargins = UIEdgeInsetsZero
-*/
-        //styleCell(cell.contentView, user: user)
-        //cell.imageView!.image = UIImage(named: "podcasts")
-        
-        if let userImageFile = user.objectForKey("photo") as? PFFile {
-            userImageFile.getDataInBackgroundWithBlock {
-                (imageData: NSData?, error: NSError?) -> Void in
-                let profImage = UIImage(data:imageData!)
-                cell.imageView!.image = profImage
-            }
-        } else {
-            cell.imageView!.image = UIImage(named: "podcasts")
-        }
-        
-        // cell.contentView.backgroundColor = UIColor.grayColor()
-        
         return cell
     }
 
     
     func styleCell(content: UIView, user: PFUser) {
-        let a = UIView(frame: CGRectMake(0, 0, 350, 240))
+        let profilePicture = UIImageView()
         
         if let userImageFile = user.objectForKey("photo") as? PFFile {
             userImageFile.getDataInBackgroundWithBlock {
                 (imageData: NSData?, error: NSError?) -> Void in
-                
-                    let profImage = UIImage(data:imageData!)
-                    let profilePic = UIImageView(image: profImage)
-                    let defaultPic = UIImageView(image: UIImage(named: "podcasts"))
-                    profilePic.frame = CGRect(x: CGRectGetMinX(a.bounds) + 30, y: CGRectGetMinY(a.bounds) + 10, width: defaultPic.frame.size.width+10, height: defaultPic.frame.size.height+10)
-                    profilePic.layer.cornerRadius = profilePic.frame.height/2.0
-                    profilePic.clipsToBounds = true
-                
-                    // a.addSubview(profilePic)
-                
-                var name = UILabel(frame: CGRect(x: CGRectGetMaxX(profilePic.bounds) + 60, y: CGRectGetMinY(a.bounds), width: 100, height: 50))
-                name.text = user.username
-                name.font = UIFont .boldSystemFontOfSize(18.0)
-                a.addSubview(name)
-                
-                println((user.objectForKey("location") as? PFGeoPoint)!.distanceInKilometersTo(PFUser.currentUser()?.objectForKey("location") as? PFGeoPoint))
-                var placeDistance = ((user.objectForKey("location") as? PFGeoPoint)!.distanceInKilometersTo(PFUser.currentUser()?.objectForKey("location") as? PFGeoPoint)) * 3280.84
-                
-                var distance = UILabel(frame: CGRect(x: CGRectGetMaxX(profilePic.bounds) + 60, y: CGRectGetMinY(a.bounds) + 35, width: 100, height: 20))
-                distance.text = "\(placeDistance) ft"
-                a.addSubview(distance)
-                
-                
-                var occupation = UILabel(frame: CGRect(x: CGRectGetMaxX(profilePic.bounds) + 60, y: CGRectGetMinY(a.bounds) + 55, width: 150, height: 20))
-                occupation.text = user.objectForKey("Occupation") as? String
-                a.addSubview(occupation)
-                
-                content.clipsToBounds = true
-                content.addSubview(a)
-                content.sendSubviewToBack(a)
-                content.bringSubviewToFront(profilePic)
-                
-                
-                
-                
-            
+                    profilePicture.image = UIImage(data:imageData!)!
+                profilePicture.contentMode = .ScaleAspectFill
             }
         } else {
-            let profilePic = UIImageView(image: UIImage(named: "podcasts"))
-            profilePic.layer.cornerRadius = profilePic.frame.height/2.0
-            profilePic.clipsToBounds = true
-            profilePic.frame = CGRect(x: CGRectGetMinX(a.bounds) + 30, y: CGRectGetMinY(a.bounds) + 10, width: profilePic.frame.size.width, height: profilePic.frame.size.height)
-            a.addSubview(profilePic)
-            
-            var name = UILabel(frame: CGRect(x: CGRectGetMaxX(profilePic.bounds) + 60, y: CGRectGetMinY(a.bounds), width: 100, height: 50))
-            name.text = user.username
-            name.font = UIFont .boldSystemFontOfSize(18.0)
-            a.addSubview(name)
-            
-            
-            println((user.objectForKey("location") as? PFGeoPoint)!.distanceInKilometersTo(PFUser.currentUser()?.objectForKey("location") as? PFGeoPoint))
-            var placeDistance = ((user.objectForKey("location") as? PFGeoPoint)!.distanceInKilometersTo(PFUser.currentUser()?.objectForKey("location") as? PFGeoPoint)) * 3280.84
-            
-            var distance = UILabel(frame: CGRect(x: CGRectGetMaxX(profilePic.bounds) + 60, y: CGRectGetMinY(a.bounds) + 35, width: 100, height: 20))
-            if placeDistance == 1 {
-                distance.text = "\(placeDistance) foot"
-            } else {
-                distance.text = "\(placeDistance) feet"
-            }
-            a.addSubview(distance)
-            
-            
-            var occupation = UILabel(frame: CGRect(x: CGRectGetMaxX(profilePic.bounds) + 60, y: CGRectGetMinY(a.bounds) + 55, width: 150, height: 20))
-            occupation.text = user.objectForKey("Occupation") as? String
-            a.addSubview(occupation)
-            content.clipsToBounds = true
-            content.addSubview(a)
-            content.sendSubviewToBack(a)
-            content.bringSubviewToFront(profilePic)
+            profilePicture.image = UIImage(named: "podcasts")
+            profilePicture.contentMode = .ScaleAspectFit
         }
         
+        profilePicture.frame = CGRectMake(22, content.frame.height/2.0 - 10, 60, 60)
+        profilePicture.layer.cornerRadius = profilePicture.frame.height/2.0
+        profilePicture.clipsToBounds = true
+        content.addSubview(profilePicture)
         
+        var nameLabel = UILabel(frame: CGRectMake(100, profilePicture.frame.origin.y, 200, 30))
+        nameLabel.text = user.username
+        nameLabel.font = UIFont.boldSystemFontOfSize(18)
+        content.addSubview(nameLabel)
         
+        var distance = ((user.objectForKey("location") as? PFGeoPoint)!.distanceInKilometersTo(PFUser.currentUser()?.objectForKey("location") as? PFGeoPoint)) * 3280.84
+        var distanceLabel = UILabel(frame: CGRectMake(100, nameLabel.frame.origin.y + 24, 200, 20))
+        distanceLabel.text = "\(distance) ft"
+        distanceLabel.font = UIFont.systemFontOfSize(16)
+        content.addSubview(distanceLabel)
+        
+        if let occupation = user.objectForKey("Occupation") as? String {
+            var occupationLabel = UILabel(frame: CGRectMake(100, distanceLabel.frame.origin.y + 14, 200, 20))
+            occupationLabel.text = occupation
+            occupationLabel.textColor = UIColor.lightGrayColor()
+            occupationLabel.font = UIFont.systemFontOfSize(14)
+            content.addSubview(occupationLabel)
+        }
+    
     }
     
-    func getAllUsers() {
+    func fetchUsers() {
         let value = NSArray()
         let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
         dispatch_async(dispatch_get_global_queue(priority, 0)) {
@@ -210,9 +146,10 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             if user?.objectForKey("genderFilter") as! String != "Both"{
                 query!.whereKey("gender", matchesRegex: (user?.objectForKey("genderFilter") as! String))
             }
-
+            /*
             let kilometers = (user?.objectForKey("distanceFilter") as! Double) / 3280.84
             query!.whereKey("location", nearGeoPoint: user?.objectForKey("location") as! PFGeoPoint, withinKilometers: kilometers)
+            */
 
             
             /*query.findObjectsInBackgroundWithBlock{
@@ -236,15 +173,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        //println(indexPath.row)
-        //let segueID = UIStoryboardSegue(identifier: "profileSegue", source: self, destination: ProfileVC.self)
-        
-        
         let cell = tableView.cellForRowAtIndexPath(indexPath)
         performSegueWithIdentifier("profileSegue", sender: cell)
-        
-        
-        
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
@@ -263,14 +193,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         initTableView()
-        getAllUsers()
+        fetchUsers()
     }
     
     override func viewDidAppear(animated: Bool) {
         if PFUser.currentUser() == nil {
             presentMainVC()
         }
-        getAllUsers()
+        fetchUsers()
     }
     
     func presentMainVC () {
@@ -279,8 +209,6 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
         let cell = sender as! UITableViewCell
         let path = tableView.indexPathForCell(cell)
         let destination = segue.destinationViewController as! ProfileVC
