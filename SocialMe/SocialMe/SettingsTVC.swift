248d4f2b91b5c0f37d9759b6e279dd949542c158
ghanbarim <ghanbari@stanford.edu>
2015-05-27 05:34:20 -0700

    enters keyword in database

--- SocialMe/SocialMe/SettingsTVC.swift
+++ SocialMe/SocialMe/SettingsTVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Parse
 
-class SettingsTVC: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class SettingsTVC: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
 
     
     @IBOutlet weak var lowerAge: UIPickerView!
@@ -17,7 +17,10 @@ class SettingsTVC: UITableViewController, UIPickerViewDelegate, UIPickerViewData
     @IBOutlet weak var distanceValue: UILabel!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var segment: UISegmentedControl!
+    @IBOutlet weak var keyword: UITextField!
 
+
+    
     
     var pickerData = [18]
     
@@ -26,6 +29,66 @@ class SettingsTVC: UITableViewController, UIPickerViewDelegate, UIPickerViewData
         distanceValue.text = "\(distance) ft"
 
     }
+    
+    @IBAction func addKey(sender: UITextField) {
+        let input = sender.text
+        
+        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            let user = PFUser.currentUser()
+            //println("Received")
+        
+            var query = PFQuery(className: "KeywordFilter")
+            query.whereKey("username", equalTo: user!.username!)
+            var objectArr = query.findObjects() as! [PFObject]
+            if objectArr.count > 0 {
+                var keyObj = objectArr[0]
+                if var basic = keyObj["basicFilter"] as? NSMutableArray {
+                    if !basic.containsObject(input){
+                        basic.addObject(input) 
+                        keyObj["basicFilter"] = basic
+                        keyObj.save()
+                    }
+                }
+            } else {
+                var newKey = PFObject(className: "KeywordFilter")
+                newKey["username"] = user?.username
+                var basic = NSMutableArray()
+                basic.addObject(input)
+                newKey["basicFilter"] = basic
+                newKey.save()
+            }
+        }
+        sender.text = ""
+        
+
+        
+        
+        /*
+        var query = PFQuery(className: "Friendships")
+        query.whereKey("username", equalTo: username)
+        var objectArr = query.findObjects() as! [PFObject]
+        if objectArr.count > 0 {
+        var friendshipObj = objectArr[0]
+        if var friendsArr = friendshipObj["friends"] as? NSMutableArray {
+        friendsArr.addObject(friendData)
+        friendshipObj["friends"] = friendsArr
+        friendshipObj.save()
+        }
+        } else {
+        var newFriendship = PFObject(className: "Friendships")
+        newFriendship["username"] = username
+        var friendsArr = NSMutableArray()
+        friendsArr.addObject(friendData)
+        newFriendship["friends"] = friendsArr
+        newFriendship.save()
+        }
+        */
+        
+
+    }
+
+    
     
     @IBAction func segmentValueChange(sender: UISegmentedControl) {
     }
@@ -98,6 +161,12 @@ class SettingsTVC: UITableViewController, UIPickerViewDelegate, UIPickerViewData
             navigationController?.popViewControllerAnimated(true)
         }
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        keyword.resignFirstResponder()
+        keyword.endEditing(true)
+        return false
+    }
 
     
     override func viewDidLoad() {
@@ -124,11 +193,44 @@ class SettingsTVC: UITableViewController, UIPickerViewDelegate, UIPickerViewData
             default:
                 segment.selectedSegmentIndex = 2
         }
+        keyword.delegate = self
+        /*
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleKeyboardWillShowNotification:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleKeyboardWillHideNotification:", name: UIKeyboardWillHideNotification, object: nil)
+    */
+    }
+    /*
+    func handleKeyboardWillShowNotification(notification: NSNotification) {
+        keyboardWillChangeFrameWithNotification(notification, showsKeyboard: true)
+    }
+    
+    func handleKeyboardWillHideNotification(notification: NSNotification) {
+        keyboardWillChangeFrameWithNotification(notification, showsKeyboard: false)
+    }
+    
+    func keyboardWillChangeFrameWithNotification(notification: NSNotification, showsKeyboard: Bool) {
+        let userInfo = notification.userInfo!
+        
+        let animationDuration: NSTimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+        
+        let keyboardScreenBeginFrame = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        
+        let keyboardViewBeginFrame = view.convertRect(keyboardScreenBeginFrame, fromView: view.window)
+        let keyboardViewEndFrame = view.convertRect(keyboardScreenEndFrame, fromView: view.window)
+        let originDelta = keyboardViewEndFrame.origin.y - keyboardViewBeginFrame.origin.y
 
         
+        view.setNeedsUpdateConstraints()
         
+        UIView.animateWithDuration(animationDuration, delay: 0, options: .BeginFromCurrentState, animations: {
+            self.view.layoutIfNeeded()
+            }, completion: nil)
         
-    }
+        // Scroll to the selected text once the keyboard frame changes.
+        // let selectedRange = textView.selectedRange
+        // textView.scrollRangeToVisible(selectedRange)
+    }*/
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
